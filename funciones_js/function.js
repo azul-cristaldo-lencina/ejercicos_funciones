b16b89261fb2c010c2d40e2e3a5a2194a7fcc9fc
azul <you@example.com>
2023-04-08 20:23:51 -0300

    descomentando ejercicios

--- funciones_js/function.js
+++ funciones_js/function.js
@@ -1,18 +1,18 @@
 // 1- Crear una función que reciba un número por parámetro e indique en consola si el número es par o impar
-/*
-function num(n){
-    if(n % 2 == 0){
+
+function num(n1){
+    if(n1 % 2 == 0){
         console.log("El número es par")
     }else{
         console.log("El número es impar")
     }
 }
-num(25)
-*/
+
+
 
 //Crear una función que reciba dos números por parámetro e indique en consola que número es mayor, y si ninguno lo es, indicar por consola que son iguales
 
-/*
+
 function numeros(a,b){
     if(a > b){
         console.log(a + " es mayor que " + b);
@@ -23,72 +23,69 @@ function numeros(a,b){
         console.log("Los número son iguales")
     }
 }
-numeros(1,2)
-*/
 
 
 
 // 3- Crear una función que reciba un número por parámetro e indique en consola si ese número es múltiplo de 5.
-/*
-function multiplo(n){
-    if(n % 5 == 0){
+
+function multiplo(n2){
+    if(n2 % 5 == 0){
         console.log("El número indicado es multiplo de 5")
     }
 }
-multiplo(2)
-*/
+
+
 
 //Crear una función que reciba un número por parámetro e imprima por consola todos los números desde el 0 hasta llegar a ese número
 
-/*
-function lista(n){
-    for(var i = 0; i <= n; i++){
+
+function lista(n3){
+    for(var i = 0; i <= n3; i++){
         console.log(i)
     }
 }
-lista(6)
-*/
+
+
 
 // Crear una función que reciba una palabra y un número por parámetro e imprima por consola  esa palabra la cantidad correspondiente al número indicado  
 
- /*function palabra(p,n){
-    for(var i=0; i < n; i++){
-        if(n * p){
+function palabra(p,n4){
+    for(var i=0; i < n4; i++){
+        if(n4 * p){
             break;
         }
         console.log(p)
     }
 }
-palabra("Hola" , 7)*/
+
 
 // 6 - Crear una función que reciba un array por parámetro e imprima por consola todos los valores de ese array.
 
-/*function array(n){
-    for(var i = 0; i < n.length; i++){
-        console.log(n[i])
+function array(n5){
+    for(var i = 0; i < n5.length; i++){
+        console.log(n5[i])
     }
 }
-array([1,3,0.4,5])
-*/
+
 
 // 7 - Crear una función que reciba un array con 10 números e imprima por consola todos los valores de ese array, menos el que se encuentre en la 5ta posición del mismo. Ayuda: Recuerden que el primer índice de un array es "0".
 
-// function array10(arr){
-//     for(var i = 0; i < arr.length; i++){
-//         if(i === 4){
-//             continue;
-//         }
-//         console.log(arr[i])
-//     }
-// }
-// array10([2,3,6,3,1,3,5,3,5,4])
+ function array10(arr){
+     for(var i = 0; i < arr.length; i++){
+        if(i === 4){
+            continue;
+        }
+         console.log(arr[i])
+     }
+ }
+
 
 
 // 8 - Crea una función que reciba un array de números y un número por parámetro e imprima por consola cada número del array multiplicado por el número pasado por parámetro.
 
-/*function array2(arr, n){
+function array2(arr, n6){
     for(var i = 0; i < arr.length; i++){
-        console.log(arr[i] *n)
+        console.log(arr[i] *n6)
     }
 }
-array2([20,6,5], 2)*/
+
